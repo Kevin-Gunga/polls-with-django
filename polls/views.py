b5aa4d0c5c39ed53f92e69288823b56453ed12ad
kevin <kevin@kali.kevin>
2024-07-11 13:46:53 +0300

    updated projs

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,25 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Choice, Question
+from django.utils.datastructures import MultiValueDictKeyError
 
-# Create your views here.
 def index(request):
-    return HttpResponse("Hello, world.The first poll.")
+    if request.method == 'POST':
+        try:
+            question = request.POST['question_text']
+            publication = request.POST['pub_date']
+            choice = request.POST['choice_text']
+            votes = request.POST['votes']
+
+            new_question = Question(question_text=question, pub_date=publication)
+            new_question.save()
+
+            new_choice = Choice(choice_text=choice, votes=votes, question=new_question)
+            new_choice.save()
+            
+            return HttpResponse("Question and choice saved successfully.")
+        
+        except MultiValueDictKeyError as e:
+            return HttpResponse(f"Missing field: {str(e)}")
+    
+    return render(request, 'polls/index.html', {})
